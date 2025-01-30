90d92bdc7385e18adf75e7d6d5eb085f701ada5e
Petr Janik <peta.janik@email.cz>
2025-01-30 21:32:38 +0100

    Add reward filters

--- src/pages/rewards/RewardList.tsx
+++ src/pages/rewards/RewardList.tsx
@@ -5,19 +5,37 @@ import {
   ReferenceField,
   SelectField,
   TextField,
+  TextInput,
 } from "react-admin";
 import { statusTypes } from "./statusTypes.ts";
 
-const RewardList = () => (
-  <List>
-    <Datagrid>
-      <TextField source="id" />
-      <TextField source="name" />
-      <ReferenceField source="user_id" reference="users" />
-      <SelectField source="status" choices={statusTypes} />
-      <DateField source="updated_at" showTime />
-    </Datagrid>
-  </List>
-);
+const RewardList = () => {
+  const rewardFilters = [
+    <TextInput
+      key="user_email_filter"
+      source="user_email"
+      alwaysOn
+      label="User email"
+    />,
+    <TextInput
+      key="user_id_filter"
+      source="user_email"
+      alwaysOn
+      label="User ID"
+    />,
+  ];
+
+  return (
+    <List filters={rewardFilters}>
+      <Datagrid>
+        <TextField source="id" />
+        <TextField source="name" />
+        <ReferenceField source="user_id" reference="users" />
+        <SelectField source="status" choices={statusTypes} />
+        <DateField source="updated_at" showTime />
+      </Datagrid>
+    </List>
+  );
+};
 
 export default RewardList;
