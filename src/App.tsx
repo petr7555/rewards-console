bdefde0cda7244c9cee6c1a51055c90467efc000
Petr Janik <peta.janik@email.cz>
2025-01-30 23:18:33 +0100

    Add theme that does not break inputs

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,24 @@ import ProgrammeList from './pages/programmes/ProgrammeList.tsx';
 import ProgrammeCreate from './pages/programmes/ProgrammeCreate.tsx';
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import PersonIcon from '@mui/icons-material/Person';
+import { defaultTheme } from 'react-admin';
+import { deepmerge } from '@mui/utils';
+
+const futureTheme = deepmerge(defaultTheme, {
+  palette: {
+    primary: {
+      main: '#a71930',
+    },
+    secondary: {
+      main: '#a71930',
+    },
+  },
+});
 
 export const App = () => (
   <Admin
     layout={Layout}
+    theme={futureTheme}
     dataProvider={dataProvider}
     authProvider={authProvider}
   >
